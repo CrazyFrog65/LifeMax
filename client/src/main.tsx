250d8d743d723ad040a05f196724a6965af08d03
atharv <sonar.atharv@gmail.com>
2026-07-14 14:55:11 +0530

    fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
+
+// Intercept requests to replace localhost API URL with production API URL if set in env
+axios.interceptors.request.use((config) => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  if (apiUrl && config.url) {
+    // Trim trailing slash from apiUrl if present to prevent double slashes
+    const trimmedApiUrl = apiUrl.endsWith('/') ? apiUrl.slice(0, -1) : apiUrl;
+    config.url = config.url.replace('http://localhost:5000', trimmedApiUrl);
+  }
+  return config;
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
